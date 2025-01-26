1173bec3fa29c5a518a433c2b25d0bab77204953
CALEB AMPONG <campong004@st.ug.edu.gh>
2025-01-26 06:44:22 +0000

    add LoginSignup component with input fields and icons

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './LoginSignup.css'
+import user_icon from '../Assets/person.png'
+import password_icon from '../Assets/password.png'
+import email_icon from '../Assets/email.png'
+
 export const LoginSignup = () => {
   return (
-    <div>
-
+    <div classname='container'>
+      <div className="header">
+        <div className="text"> Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="" />
+          <input type="name" />
+        </div>
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" />
+        </div>
+        <div className="input">
+          <img src= {password_icon} alt="" />
+          <input type="password" />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="Confirm Password" />
+        </div>
+      </div>
 
     </div>
   )
